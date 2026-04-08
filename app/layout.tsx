23d00cb8e226af73f3940213ba32569a135c3e42
SAMEEHA ALKURDI (soma) <soma7agha@gmail.com>
2026-04-08 12:43:43 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,12 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+ 
+  title: 'CoinKeep - DEV Agency',
+  description: 'Professional Wealth Management by SOMA',
+  generator: 'SOMA', 
   icons: {
     icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
       {
         url: '/icon.svg',
         type: 'image/svg+xml',
@@ -36,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+     
       <body className="font-sans antialiased">
         {children}
         <Analytics />
